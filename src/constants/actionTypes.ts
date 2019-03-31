83c19580a9a3029b217d9cddf88457e525215cef
Sirajul Muneer C B <siraj.mussafirr@gmail.com>
2019-04-01 01:41:50 +0530

    Remove unwanted code

--- src/constants/actionTypes.ts
+++ src/constants/actionTypes.ts
@@ -1,16 +1,14 @@
-
+// Used for Fetch Action
 export const FETCH_MARKERS = 'FETCH_MARKERS';
 export const FETCH_MARKERS_SUCCESS = 'FETCH_MARKERS_SUCCESS';
 export const FETCH_MARKERS_ERROR = 'FETCH_MARKERS_ERROR';
 
+// Used for Create/Update Action
 export const ADD_MARKER = 'ADD_MARKER';
 export const ADD_MARKER_SUCCESS = 'ADD_MARKER_SUCCESS';
 export const ADD_MARKER_ERROR = 'ADD_MARKER_ERROR';
 
-// export const UPDATE_MARKER = 'UPDATE_MARKER';
-// export const UPDATE_MARKER_SUCCESS = 'UPDATE_MARKER_SUCCESS';
-// export const UPDATE_MARKER_ERROR = 'UPDATE_MARKER_ERROR';
-
+// Used for Delete Action
 export const DELETE_MARKER = 'DELETE_MARKER';
 export const DELETE_MARKER_SUCCESS = 'DELETE_MARKER_SUCCESS';
 export const DELETE_MARKER_ERROR = 'DELETE_MARKER_ERROR';
